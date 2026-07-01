bf2fa1871bbefccc17f7d92fc976b14c7bc933c0
arsi <arswebstierr@gmail.com>
2026-07-01 10:44:10 +0300

    Remove unused trajectory buffering wrapper

--- reward_composition_api/wrappers/__init__.py
+++ reward_composition_api/wrappers/__init__.py
@@ -3,13 +3,10 @@ from __future__ import annotations
 from .atari import AtariFireResetEnv
 from .lunar_lander import LunarLanderSaveInfo
 from .preference_reward import BaseLearnedRewardRuntime, BasePreferenceRewardWrapper
-from .trajectory_buffering import BufferingWrapper, Trajectory
 
 __all__ = [
     "AtariFireResetEnv",
     "BaseLearnedRewardRuntime",
     "BasePreferenceRewardWrapper",
-    "BufferingWrapper",
     "LunarLanderSaveInfo",
-    "Trajectory",
 ]
